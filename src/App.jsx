7fd11e2ca4dcf63b01a456c7a848e622ee714adf
Berlin Ortega <marlitros200002@gmail.com>
2024-12-12 14:30:36 -0600

    adf

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-import Gato1 from ".//components/gato";
-import Gato2 from ".//components/Gato2";
-import Gato3 from ".//components/Gato3";
+import Gato from ".//components/gato";
 import './css/index.css'
+import Gato1 from './images/Gato1.png'
+import Gato2 from './images/Gato2.png'
+import Gato3 from './images/Gato3.png'
+
 
 function App() {
   return (
     <main>
-      <Gato1 />
-      <Gato2 />
-      <Gato3 />
+      <Gato 
+        img = {Gato1}
+        name = "Mr. Whiskerson"
+        phone = "(212) 555-1234"
+        email = "mr.whiskaz@natnap.meow"
+      />
+      <Gato
+        img = {Gato2}
+        name = "Fluffykins"
+        phone = "(212) 555-2412"
+        email = "flufff@me.com"
+      />
+      <Gato
+        img = {Gato3}
+        name = "Pusskins"
+        phone = "(212) 555-6666"
+        email = "thecat@hotmail.com"
+      />
     </main>
   )
 }
--- src/components/gato.jsx
+++ src/components/gato.jsx
@@ -3,21 +3,21 @@ import MessageIcon from '../images/MessageIcon.png'
 import PhoneIcon from '../images/PhoneIcon.png'
 import '../css/index.css'
 
-export default function Gato() {
+export default function Gato(props) {
     return(
         <div className="Gato1Div">
             <div className="Gato1Img">
-                <img src={Gato1} alt="Gato1" />
+                <img src={props.img} alt={`Foto de ${props.name}`} />
             </div>
             <div className="Gato1Info">
-                <h1>Mr. Whiskerson</h1>
+                <h1>{props.name}</h1>
                     <div>
                         <img src={PhoneIcon} alt="PhoneIcon" />
-                        <p>(212) 555-1234</p>
+                        <p>{props.phone}</p>
                     </div>
                     <div>
                         <img src={MessageIcon} alt="Messageicon" />
-                        <p>mr.whiskax@catnap.meow</p>
+                        <p>{props.email}</p>
                     </div>
             </div>
         </div>
